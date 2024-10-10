bdc9301dd8adc8679d9b97210a603d3cd086db4d
İhsan Mirage <tezravasen@gmail.com>
2024-10-10 15:00:59 +0300

    feat: complete blog for the website

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,9 @@ import { sanityClient, urlFor } from '../lib/sanity';
 import Link from 'next/link';
 import Image from 'next/image';
 import simpleBlogCard from '../lib/interface';
-import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card';
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './components/card';
+import { Button, buttonVariants } from './components/button';
+export const revalidate = 30;
 
 async function getData(): Promise<simpleBlogCard[]> {
   const query = `*[_type=='blog'] | order(_createdAt desc) {
@@ -29,8 +31,13 @@ export default async function Blog() {
             className="rounded-t-lg h-[200px] object-cover"
           />
           <CardContent className="mt-5>">
-            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
             <p className="line-clamp-3 text-sm mt-2 text-gray-600">{post.smallDescription}</p>
+          <Button asChild className="w-full mt-8">
+            <Link href={`/blog/makaleler/${post.currentSlug}`}>
+              Makaleyi Oku
+            </Link>
+          </Button>
           </CardContent>
         </Card>
       ))}
--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,6 +1,13 @@
+import { PortableTextBlock } from '@portabletext/types';
 export default interface simpleBlogCard{
     title: string;
     smallDescription: string;
     titleImage: { asset: { url: string } };
     currentSlug: string;
-}
+}  
+export interface fullBlog {
+    currentSlug: string;
+    title: string;
+    content: any;
+    titleImage: { asset: { url: string } };
+  }

c6b62097151a2cfea4f63d39ce7867ec6034a916
İhsan Mirage <tezravasen@gmail.com>
2024-11-08 15:55:07 +0300

    style: refactor styling of pagecontents component

--- app/blog/components/PageContents.tsx
+++ app/blog/components/PageContents.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { slugify } from "@/lib/helpers";
 
 const PageContents = ({ headings }: any) => (
-  <div className="max-w-2xl mx-auto mt-8 text-center border rounded-sm dark:border-purple-950 ">
-    <h2 className="text-xl font-bold p-2 mb-5 border-b dark:border-purple-950 bg:amber-50 dark:bg-slate-900/20">
+  <div className="max-w-2xl mx-auto mt-8  text-left border rounded-sm dark:border-purple-950 ">
+    <h2 className="text-xl text-center font-bold p-2 mb-5 border-b dark:border-purple-950 bg:amber-50 dark:bg-slate-900/20">
       İçerik
     </h2>
     <nav>
-      <ul>
+    <ul className="ps-5">
         {headings?.map((heading: any) => (
-          <li key={heading?._key} className="mb-2">
+          <li key={heading?._key} className="my-4 list-disc">
             <a
               className="hover:underline"
               href={`#${slugify(heading.children[0].text)}`}

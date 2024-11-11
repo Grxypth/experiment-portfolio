f8828845fce932268bd5f80c00c2d0f093f46faa
İhsan Mirage <tezravasen@gmail.com>
2024-11-11 09:50:23 +0300

    refactor: center blog logo

--- app/blog/components/BlogNavbar.tsx
+++ app/blog/components/BlogNavbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function BlogNavbar() {
   return (
-    <div className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+    <div className="w-full relative flex items-center justify-center max-w-2xl mx-auto  py-5">
       <Link href="/" className="font-bold text-3xl">
         Ihsan<span className="text-primary">Erdem</span>
       </Link>
